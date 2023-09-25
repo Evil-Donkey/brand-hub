65b6843792e2ba86a622d7d3aef1bb53c81e1b7d
evilJeff <jeff@evildonkey.co.uk>
2023-09-25 11:31:09 +0200

    add third column

--- src/app/components/FlexibleContent/TwoColumns/index.js
+++ src/app/components/FlexibleContent/TwoColumns/index.js
@@ -4,6 +4,8 @@ import styles from './TwoColumns.module.scss'
 
 const TwoColumns = ({ data }) => {
     const blocks = data?.blocks;
+    const n = parseInt(blocks.length);
+    const colClass = (n % 3 == 0) ? 'col-md-4' : 'col-md-6';
 
     return blocks && (
         <div className='row justify-content-end'>
@@ -11,7 +13,7 @@ const TwoColumns = ({ data }) => {
                 const { altText, mediaItemUrl, mediaDetails, sizes } = block?.image;
                 const copy = block?.copy;
                 return block && (
-                    <div key={i.toString()} className='col-md-6 mb-5'>
+                    <div key={i.toString()} className={`${colClass} mb-5`}>
                         {mediaItemUrl &&
                             <div className={styles.assetImageWrap}>
                                 <Image 
