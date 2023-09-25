de8f06804645f41571103c32b0d3ef0e66068c04
evilJeff <jeff@evildonkey.co.uk>
2023-09-25 15:30:37 +0200

    GTM setup

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,11 @@
-import GoogleAnalytics from './components/GoogleAnalytics'
+import Script from 'next/script'
+// import GoogleAnalytics from './components/GoogleAnalytics'
+import GoogleTagManager from './components/GoogleAnalytics'
 import { robotoFlex, robotoSlab } from './utils/fonts'
 import 'bootstrap/dist/css/bootstrap.css'
 import './globals.css'
+
+const GTM_ID = process.env.GTM_ID
 
 export const metadata = {
   title: {
@@ -13,9 +17,15 @@ export const metadata = {
 export default function RootLayout({ children, params }) {
   return (
     <html lang="en">
-      <GoogleAnalytics GA_TRACKING_ID={process.env.GA_TRACKING_ID} />
+      {/* <GoogleAnalytics GA_TRACKING_ID={process.env.GA_TRACKING_ID} /> */}
+      <GoogleTagManager GTM_ID={GTM_ID} />
       <body className={`${robotoSlab.variable} ${robotoFlex.className}`}>
         {children}
+        <noscript
+            dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+            }} 
+        />
       </body>
     </html>
   )
