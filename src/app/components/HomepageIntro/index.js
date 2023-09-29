dec810867b041414de2435357850ad49ca989536
evilJeff <jeff@evildonkey.co.uk>
2023-09-29 11:04:24 +0200

    removed target gtm

--- src/app/components/HomepageIntro/index.js
+++ src/app/components/HomepageIntro/index.js
@@ -6,21 +6,22 @@ import { trackEvent } from '../GoogleTagManager';
 
 const Intro = ({ content, email, telephone, title }) => {
 
-    const trackDemo = () => {
-        trackEvent({
-          event: 'demoClick'
-        });
-    };
-    const trackCall = () => {
-        trackEvent({
-          event: 'callClick'
-        });
-    };
-    const trackEmail = () => {
-        trackEvent({
-          event: 'emailClick'
-        });
-    };
+    // const trackDemo = () => {
+    //     trackEvent({
+    //       event: 'demoClick'
+    //     });
+    // };
+    // const trackCall = () => {
+    //     trackEvent({
+    //       event: 'callClick'
+    //     });
+    // };
+    // const trackEmail = () => {
+    //     trackEvent({
+    //       event: 'emailClick'
+    //     });
+    // };
+    
     return (
         <div className={styles.introContainer}>
             <div className={`${styles.introContainer} container`}>
@@ -33,9 +34,9 @@ const Intro = ({ content, email, telephone, title }) => {
                     <div className='col-md-5'>
                         {content && <div dangerouslySetInnerHTML={{ __html: content }} />}
                         <div className={`d-flex flex-column flex-lg-row gap-lg-3 align-items-center ${!content ? 'justify-content-end' : ''}`}>
-                            <Link href="#form-request" className="cta__btn mt-3" onClick={() => trackDemo()}>Request demo</Link>
-                            {telephone && <a href={`tel:${telephone}`} className="cta__btn cta__btn--transparent mt-3" target="_blank" onClick={() => trackCall()}>Call us</a>}
-                            {email && <a href={`mailto:${email}`} className="cta__btn cta__btn--transparent mt-3" target="_blank" onClick={() => trackEmail()}>Email us</a>}
+                            <Link href="#form-request" className="cta__btn mt-3">Request demo</Link>
+                            {telephone && <a href={`tel:${telephone}`} className="cta__btn cta__btn--transparent mt-3" target="_blank">Call us</a>}
+                            {email && <a href={`mailto:${email}`} className="cta__btn cta__btn--transparent mt-3" target="_blank">Email us</a>}
                         </div>
                     </div>
                 </div>
