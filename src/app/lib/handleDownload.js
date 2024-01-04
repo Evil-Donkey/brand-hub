f116c4157a44df93c1cc13b72fcc7b3aa54ec010
evilJeff <jeff@evildonkey.co.uk>
2024-01-04 18:49:57 +0100

    CORS

--- src/app/lib/handleDownload.js
+++ src/app/lib/handleDownload.js
@@ -1,7 +1,25 @@
+// import { saveAs } from 'file-saver'
+
+// export default function handleDownload(url, filename) {
+//     fetch(url)
+//         .then(res => res.blob())
+//         .then(blob => saveAs(blob, filename))
+// }
+
 import { saveAs } from 'file-saver'
 
 export default function handleDownload(url, filename) {
-    fetch(url)
-        .then(res => res.blob())
-        .then(blob => saveAs(blob, filename))
+    fetch(url, {
+        method: 'GET',
+        mode: 'cors', // CORS mode
+        // credentials: 'include' // Include credentials for cross-origin requests
+    })
+    .then(res => {
+        if (!res.ok) {
+            throw new Error(`Server responded with ${res.status}: ${res.statusText}`);
+        }
+        return res.blob();
+    })
+    .then(blob => saveAs(blob, filename))
+    .catch(error => console.error('Download error:', error));
 }
