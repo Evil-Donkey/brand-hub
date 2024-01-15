38cddeb58f6f35a8db5cc6762cdd7f9580e2c1e5
evilJeff <jeff@evildonkey.co.uk>
2024-01-15 14:15:56 +0100

    Add share functionality

--- src/app/components/BranderHero/index.js
+++ src/app/components/BranderHero/index.js
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import Link from "next/link"
+import Share from "../Share"
 
 const BranderHero = ({ content, featuredImage, title }) => {
     
@@ -13,13 +14,14 @@ const BranderHero = ({ content, featuredImage, title }) => {
                 }
             </div>
             <div className='row justify-content-between'>
-                <div className="col-md-5">
-                    <Link href="/branders">
+                <div className="col-md-5 mb-5 d-flex flex-md-column align-items-start">
+                    <Link href="/branders" className="me-4 me-md-0 mb-md-4">
                         <svg xmlns="http://www.w3.org/2000/svg" width="43" height="43" viewBox="0 0 43 43" fill="none">
                         <rect x="-0.00683594" width="42.4888" height="42.4888" rx="5" fill="white"/>
                         <path d="M24.8682 11L15.1181 21.0001L24.8682 31.0002" stroke="#231F20" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                         </svg>
                     </Link>
+                    <Share />
                 </div>
                 {content &&
                     <div className='col-md-5'>
---
+++ src/app/components/Share/index.js
@@ -0,0 +1,32 @@
+'use client'
+
+import { usePathname } from 'next/navigation';
+import {
+    FacebookShareButton,
+    LinkedinShareButton,
+    TwitterShareButton,
+    FacebookIcon,
+    LinkedinIcon,
+    XIcon
+} from "react-share";
+
+const Share = () => {
+
+    const pathname = usePathname();
+    
+    return (
+        <div className='d-flex flex-md-column gap-2'>
+            <LinkedinShareButton url={pathname}>
+                <LinkedinIcon size={42} borderRadius={8} bgStyle={{ fill: 'white' }} iconFillColor={'#131111'} />
+            </LinkedinShareButton>
+            <FacebookShareButton url={pathname}>
+                <FacebookIcon size={42} borderRadius={8} bgStyle={{ fill: 'white' }} iconFillColor={'#131111'} />
+            </FacebookShareButton>
+            <TwitterShareButton url={pathname}>
+                <XIcon size={42} borderRadius={8} bgStyle={{ fill: 'white' }} iconFillColor={'#131111'} />
+            </TwitterShareButton>
+        </div>
+    );
+}
+
+export default Share;
