eea209f709d5c8737c2657a9efdde4777f2dc312
evilJeff <jeff@evildonkey.co.uk>
2024-02-21 10:41:35 +0100

    branders article columns update

--- src/app/components/BranderContent/index.js
+++ src/app/components/BranderContent/index.js
@@ -9,34 +9,38 @@ const BranderContent = ({ content }) => {
         <div className={styles.branderContent}>
             <div className='py-5 container'>
                 <div className="row">
-                    {branderPhoto &&
-                        <div className="col-md-6">
-                            <Image
-                                src={branderPhoto?.mediaItemUrl}
-                                width={branderPhoto?.mediaDetails?.width}
-                                height={branderPhoto?.mediaDetails?.height}
-                                alt={branderPhoto?.altText}
-                            />
-                        </div>
-                    }
-                    {branderQuote &&
-                        <div className="col-md-6 ps-md-5 mb-5">
-                            <h2>{branderQuote}</h2>
-                            <h4>Brander</h4>
-                            <h3>{branderName}</h3>
-                        </div>
-                    }
+                    <div className={`col d-flex flex-column flex-md-row ${styles.articleContentWrapper}`}>
+                        {branderPhoto &&
+                            <div className={`${styles.branderImage} ${styles.articleColumn}`}>
+                                <Image
+                                    src={branderPhoto?.mediaItemUrl}
+                                    width={branderPhoto?.mediaDetails?.width}
+                                    height={branderPhoto?.mediaDetails?.height}
+                                    alt={branderPhoto?.altText}
+                                />
+                            </div>
+                        }
+                        {branderQuote &&
+                            <div className={styles.articleColumn}>
+                                <h2>{branderQuote}</h2>
+                                <h4>Brander</h4>
+                                <h3>{branderName}</h3>
+                            </div>
+                        }
+                    </div>
                 </div>
 
                 {copyColumns &&
                     <div className='row'>
-                        {copyColumns.map((column, i) => {
-                            return (
-                                <div className='col-md-6 mb-4' key={i.toString()}>
-                                    <div dangerouslySetInnerHTML={{ __html: column.copy }} />
-                                </div>
-                            );
-                        })}
+                        <div className={`col d-flex flex-column flex-md-row ${styles.articleContentWrapper}`}>
+                            {copyColumns.map((column, i) => {
+                                return (
+                                    <div className={styles.articleColumn} key={i.toString()}>
+                                        <div dangerouslySetInnerHTML={{ __html: column.copy }} />
+                                    </div>
+                                );
+                            })}
+                        </div>
                     </div>
                 }
             </div>
