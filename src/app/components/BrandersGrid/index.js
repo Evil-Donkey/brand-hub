722c10a59d9f7fd0a9efbfd1e9b567df9943c86a
evilJeff <jeff@evildonkey.co.uk>
2024-02-27 14:12:35 +0100

    Branders badge

--- src/app/components/BrandersGrid/index.js
+++ src/app/components/BrandersGrid/index.js
@@ -1,21 +1,22 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import styles from './BrandersGrid.module.scss'
 
 const BrandersGrid = ({ branders }) => {
-    
     return (branders) ? (
         <div className={`my-5 container ${styles.brandersGrid}`}>
             <div className='row'>
                 <ul className='col-12 m-0 d-flex flex-wrap d-md-grid gap-3 list-unstyled'>
                     {branders.map((brander, i) => {
-                        const { thumbnail, branderQuote, branderName } = brander.branderOptions;
+                        const { thumbnail, branderQuote, branderName, specialBadge } = brander.branderOptions;
                         const slug = brander.slug;
                         return (
                             <li key={brander.id} className='p-md-5' style={{ backgroundImage: `url(${thumbnail.mediaItemUrl})` }}>
                                 <Link href={`/branders/${slug}`} className='p-4 p-md-0 d-flex align-items-end w-100 h-100'>
                                     <div className='d-flex flex-column'>
-                                        <h2>{branderQuote}</h2>
-                                        <h3>{branderName}</h3>
+                                        {specialBadge && <Image className='mb-3' src="/images/icon-career-special.svg" width={91} height={94} alt="Career special badge" />}
+                                        {branderQuote && <h2>{branderQuote}</h2>}
+                                        {branderName && <h3>{branderName}</h3>}
                                     </div>
                                 </Link>
                             </li>
