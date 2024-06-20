cbeeb2e879b8c85529c192ffe8935dbcb0611c85
evilJeff <jeff@evildonkey.co.uk>
2024-06-20 09:29:24 +0200

    Hotjar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,14 @@
 import Script from 'next/script'
 import GoogleAnalytics from './components/GoogleAnalytics'
 import GoogleTagManager from './components/GoogleTagManager'
+import Hotjar from './components/Hotjar'
 import { anton, openSans, robotoFlex, robotoSlab } from './utils/fonts'
 import 'bootstrap/dist/css/bootstrap.css'
 import './globals.css'
 
 const GTM_ID = process.env.GTM_ID
 const GA_TRACKING_ID = process.env.GA_TRACKING_ID
+const HOTJAR_ID = process.env.HOTJAR_ID
 
 export const metadata = {
   title: {
@@ -20,6 +22,7 @@ export default function RootLayout({ children, params }) {
     <html lang="en">
       <GoogleAnalytics GA_TRACKING_ID={GA_TRACKING_ID} />
       <GoogleTagManager GTM_ID={GTM_ID} />
+      <Hotjar HOTJAR_ID={HOTJAR_ID} />
       <body className={`${robotoSlab.variable} ${anton.variable} ${openSans.variable} ${robotoFlex.className}`}>
         {children}
         <noscript
