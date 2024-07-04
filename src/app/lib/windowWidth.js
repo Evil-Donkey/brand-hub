7a993f2d7e872ec8c01997668e6cb1532650adb0
evilJeff <jeff@evildonkey.co.uk>
2024-07-04 12:02:57 +0200

    fix window is not defined error

--- src/app/lib/windowWidth.js
+++ src/app/lib/windowWidth.js
@@ -1,14 +1,17 @@
 import { useState, useEffect } from 'react';
 
 export default function useWindowWidth() {
-    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-  
-    useEffect(() => {
+  const [windowWidth, setWindowWidth] = useState(null);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
       const handleResize = () => setWindowWidth(window.innerWidth);
-  
+      handleResize(); // Set initial width
+
       window.addEventListener('resize', handleResize);
       return () => window.removeEventListener('resize', handleResize);
-    }, []);
-  
-    return windowWidth;
-  }
+    }
+  }, []);
+
+  return windowWidth;
+}
